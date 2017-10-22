d4a0fbf18b6c9dcc1ebb2630fac8226799f7bd59
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-22 13:53:40 -0200

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# speakerdeck-widget
+<h1>Speaker Deck Talks Widget</h1>
+    <p>
+      A very simple Speaker Deck Talks Widget, based on <a href="https://github.com/jcouyang/">@jcouyang</a>, <a href="https://github.com/jcouyang/gh-widget">
+Github Widget</a></p>
+
+```html
+	<div id="speakerdeck-widget" data-user="your-speakerdeck-username" data-display="talks"></div>
+	<script src="http://ezefranca.com/speakerdeck-widget/index.js"></script>
+```
+<h3>Try it :)</h3>
+<p>Go to <b>/your-speaker-deck-username</b> and see how your widget looks</p>
+
+![](screenshot.png)
+
+## Config
+- username: put your github username in `data-user` attribute
+- display: for now, use 'talks' (in the the future I planning get other things)
+
+## Try
+- https://ezefranca.com/speakerdeck-widget/<your-speakerdeck-username>
+
+## Live Example
+- http://ezefranca.com/speakerdeck-widget/index.html
+
+## How this works
+The same way of <a href="https://github.com/jcouyang/gh-widget">gh-widget</a> works.
+
---
+++ index.js
@@ -0,0 +1,32 @@
+var request = new XMLHttpRequest();
+var widget = document.getElementById('speakerdeck-widget');
+var username, pathname = location.pathname.split('/');
+if(location.hostname=='http://ezefranca.com/speakerdeck-widget/' && !!pathname[1])
+  username = pathname[1];
+else if(widget.dataset.user)
+  username = widget.dataset.user;
+
+request.overrideMimeType("application/json");  
+request.open("GET", "https://gist.github.com.ru/ezefranca/0f33a0baec388f665bdfb88a6aa77725?username=" + username);
+request.onreadystatechange = function () {
+  if (request.status != 200) return;
+  var response = JSON.parse(request.response.replace(/href=\\"\//g, 'href=\\"https://speakerdeck.com/'));
+  if (response.error) {
+    widget.innerHTML = 'Please make sure your name in "data-user" is a real person on speakerdeck';
+    return; 
+  }
+  // If you want a more quality version replace the thumbnail and use the  high qualityimagens 
+  //  widget.innerHTML = response.result.talks.replace("thumb_slide_0.jpg","slide_1.jpg");
+  widget.innerHTML = response.result.talks;
+};
+request.send();
+
+function appendCSS(name){
+  var stylesheet = document.createElement("link")
+  stylesheet.setAttribute("rel", "stylesheet")
+  stylesheet.setAttribute("href", name)
+  document.getElementsByTagName("head")[0].appendChild(stylesheet)
+}
+
+appendCSS('/main.css');
+appendCSS('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')
