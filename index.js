90c744c1a286997c200a3ff8c8af8573a94a1476
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-22 15:03:33 -0200

    fix json parser

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ request.overrideMimeType("application/json");
 request.open("GET", "https://gist.github.com.ru/ezefranca/0f33a0baec388f665bdfb88a6aa77725?username=" + username);
 request.onreadystatechange = function () {
   if (request.status != 200) return;
-  var response = request.response;//.replace(/href=\\"\//g, 'href=\\"https://speakerdeck.com/'));
+  var _response = request.response;//.replace(/href=\\"\//g, 'href=\\"https://speakerdeck.com/'));
+  var response = JSON.parse(_response);
+  console.log(response);
   if (response.error) {
     widget.innerHTML = 'Please make sure your name in "data-user" is a real person on speakerdeck';
     return; 
