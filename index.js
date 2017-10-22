4cf32f991c1a25669768033937417589bfbe8e2d
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-22 16:41:26 -0200

    rename variable

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var widget = document.getElementById('speakerdeck-widget');
 var username, pathname = location.pathname.split('/');
 if(location.hostname=='http://ezefranca.com/speakerdeck-widget/' && !!pathname[1])
   username = pathname[1];
-else if(widget.dataset.user)
-  username = widget.sp-username;
+else if(widget.dataset.username)
+  username = widget.dataset.username;
 
 request.overrideMimeType("application/json");  
 request.open("GET", "https://gist.github.com.ru/ezefranca/0f33a0baec388f665bdfb88a6aa77725?username=" + username);
