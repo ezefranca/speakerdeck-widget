00e954d681cc0df53346775ff97221874cd508e8
Ezequiel França dos Santos <ezequiel.ifsp@gmail.com>
2020-10-23 22:17:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-function appendCSS(e){var t=document.createElement("link");t.setAttribute("rel","stylesheet"),t.setAttribute("href",e),document.getElementsByTagName("head")[0].appendChild(t)}var request=new XMLHttpRequest,_widget=document.getElementById("speakerdeck-widget"),username,pathname=location.pathname.split("/");username="ezefranca.com"==location.hostname&&pathname[2]?pathname[2]:_widget.getAttribute("data-sp_username"),request.overrideMimeType("application/json"),request.open("GET","https://gist.github.com.ru/ezefranca/0f33a0baec388f665bdfb88a6aa77725?username="+username),request.onreadystatechange=function(){if(200==request.status){var e=JSON.parse(request.response.replace(/href=\\"\//g,'href=\\"https://speakerdeck.com/'));e.error?_widget.innerHTML='Please make sure your name in "username" is a real person on speakerdeck':_widget.innerHTML=e.result.talks}},request.send(),appendCSS("//ezefranca.com/speakerdeck-widget/main.css"),appendCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+function appendCSS(e) {
+    var t = document.createElement("link");
+    t.setAttribute("rel", "stylesheet"), t.setAttribute("href", e), document.getElementsByTagName("head")[0].appendChild(t);
+}
+var request = new XMLHttpRequest(),
+    _widget = document.getElementById("speakerdeck-widget"),
+    username,
+    pathname = location.pathname.split("/");
+(username = "ezequiel.app" == location.hoastname && pathname[2] ? pathname[2] : _widget.getAttribute("data-sp_username")),
+    request.overrideMimeType("application/json"),
+    request.open("GET", "https://gist.github.com.ru/ezefranca/0f33a0baec388f665bdfb88a6aa77725?username=" + username),
+    (request.onreadystatechange = function () {
+        if (200 == request.status) {
+            var e = JSON.parse(request.response.replace(/href=\\"\//g, 'href=\\"https://speakerdeck.com/'));
+            e.error ? (_widget.innerHTML = 'Please make sure your name in "username" is a real person on speakerdeck') : (_widget.innerHTML = e.result.talks);
+        }
+    }),
+    request.send(),
+    appendCSS("//ezequiel.app/speakerdeck-widget/main.css"),
+    appendCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
